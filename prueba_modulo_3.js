e622851d7bce1fa7fce11ab87b6ecc9e69aaeff8
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-07-02 20:31:28 -0400

    test de doc

---
+++ prueba_modulo_3.js
@@ -0,0 +1,5 @@
+// Un equipo de desarrollo ha sido encargado de crear una aplicación de JavaScript que permita a los usuarios realizar operaciones básicas y trabajar ocn estructuras fundamentales del lenguaje, Se busca desarrollar habilidades en el lenguaje JavaScript a través de una práctica aplicada, implementando los conceptos fundamentales del lenguaje.
+
+// desarollar una app en consola que permita : realizar oerpaciones matematicas básicas, impllementar estructuras condicionales y de bucles , usar fuciones para modularizar el código , trabajar co0n arreglos y objetos
+
+// El código debera estrar estructurado de manera clara segun las mejores practicas de JS
